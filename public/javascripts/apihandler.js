ded134f7fe9a4dfacdaa1eee9e8ba7bd76e8c8ff
manuelbarreda <manubarreda69@gmail.com>
2020-11-26 17:11:23 +0100

    Change baseURL

--- public/javascripts/apihandler.js
+++ public/javascripts/apihandler.js
@@ -3,8 +3,8 @@ class APIHandler {
     constructor() {
 
         this.axiosApp = axios.create({
-            baseURL: `http://localhost:3000/api/travels`
-            // baseURL: `https://cityapp-project.herokuapp.com/api/travels`
+            // baseURL: `http://localhost:3000/api/travels`
+            baseURL: `https://cityapp-project.herokuapp.com/api/travels`
         })
     }
 
